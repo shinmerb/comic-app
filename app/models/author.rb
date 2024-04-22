f75e25e68a74b107d6a328c8258f9a0151593d09
maimu <m0smile10often@gmail.com>
2024-04-22 22:35:57 +0900

    関連を定義

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,4 @@
 class Author < ApplicationRecord
+  has_many :comic_authors
+  has_many :comics, through: :comic_authors
 end
--- app/models/comic.rb
+++ app/models/comic.rb
@@ -1,3 +1,5 @@
 class Comic < ApplicationRecord
   belongs_to :publisher
+  has_many :comic_authors
+  has_many :authors, through: :comic_authors
 end
