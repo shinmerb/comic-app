b6b86757ac82caa397ff8f0198c3ae356d523705
maimux2x <m0smile10often@gmail.com>
2024-07-01 22:17:07 +0900

    WIP

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -2,4 +2,6 @@ class Comic < ApplicationRecord
   belongs_to :publisher
   has_many :comic_authors
   has_many :authors, through: :comic_authors
+
+  accepts_nested_attributes_for :comic_authors
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   resources :publishers
   resources :authors
+  resources :comics
 
   # Defines the root path route ("/")
   # root "posts#index"
